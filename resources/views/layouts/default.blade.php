7625fb2bffed8ee01c1954e7b796041b7dd273ac
tutuut <814146171@qq.com>
2020-07-26 16:23:05 +0800

    static cache

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,22 @@
 <html>
 <head>
     <title>@yield('title', 'Weibo App') - 我们的交友平台</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+                <li class="nav-item"><a class="nav-link" href="/about">关于</a></li>
+                <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
