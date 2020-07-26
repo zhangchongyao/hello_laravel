cfe8e940c285b35dd099f8e016d910a9e8fa9e4d
tutuut <814146171@qq.com>
2020-07-26 18:57:17 +0800

    show users/id

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    /**
+     * 显示用户个人信息
+     * @param User $user
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
